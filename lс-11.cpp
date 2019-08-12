5b0c4d3526ca923d59c15c5a31b9f044176d186f
Alexey <47507715+algres@users.noreply.github.com>
2019-08-12 05:23:22 +0300

    Update lс-11.cpp

--- lс-11.cpp
+++ lс-11.cpp
@@ -2,51 +2,39 @@
 
 using namespace std;
 
-int findMinimum (int, int);
 
 int main()
 {
 	int quantity;
-	
+
 	cout << "Enter quantity: ";
 	cin >> quantity;
 
 	if (quantity >= 2) {
 		int *array = new int[quantity];
 		cout << "Enter values: ";
-		
-		for (int i = 0; i < quantity; i++) {
+
+		for (size_t i = 0; i < quantity; i++) {
 			cin >> array[i];
 		}
 
 		int mostWater = 0;
-		for (int i = 0; i < quantity; i++) {
-			for (int j = i + 1; j < quantity; j++) {
-				int min = findMinimum(array[i], array[j]);
-				
-				if ((min * (j - i)) > mostWater) {
-					mostWater = (min * (j - i));
+		for (size_t i = 0; i < quantity; i++) {
+			for (size_t j = i + 1; j < quantity; j++) {
+				int lowestPile = array[i] < array[j] ? array[i] : array[j];
+
+				if ((lowestPile * (j - i)) > mostWater) {
+					mostWater = (lowestPile * (j - i));
 				}
 			}
 		}
-		
-		cout << "Output: " << mostWater << "\n";
+
+		cout << "Output: " << mostWater << endl;
 		delete[] array;
 	}
 	else {
-		cout << "Error: quantity must be at least 2!" << "\n";
+		cout << "Error: quantity must be at least 2!" << endl;
 	}
 
-	system("pause");
 	return 0;
-}
-
-int findMinimum (int i, int j)
-{
-	if (i < j) {
-		return i;
-	}
-	else {
-		return j;
-	}
 }
